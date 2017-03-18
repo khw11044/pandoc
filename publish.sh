bf4cd3320d5489d5e612560f8637d81086e35e01
kimhanwoong <khw7096@gmail.com>
2017-03-19 04:45:00 +0900

    내용수정

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 pandoc --toc -S --epub-stylesheet style.css --webtex -t epub3 -o pandoc.epub title.txt \
 01_Preface.md \
 02_Introduction.md \
-03_Install_Pandoc.md \
+03_Pandoc.md \
 04_Markdown.md \
 05_Epub.md \
 06_Create.md \
